8d3fd170f5f03fd49f64fdb86c5a77a3b4009254
sarahzard <nerbysarah@gmail.com>
2021-07-02 15:27:14 +0200

    Added NUnit Test Project

---
+++ NUnitTest/Card.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+
+namespace NUnitTest
+{
+    public class Card
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            B
+            Assert.Pass();
+        }
+    }
+}
